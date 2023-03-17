9f71eb2ede0a0e2701be68c538c32278fad7821a
Firos K <f>
2023-03-17 15:31:40 +0530

    Counter added

--- script.js
+++ script.js
@@ -11,7 +11,25 @@ function rand(max) {
   }
   
   
-  
+  let counter = 600;
+let counterInterval;
+
+function startCounter() {
+  counterInterval = setInterval(() => {
+    counter--;
+    if (counter === 0) {
+      clearInterval(counterInterval);
+      alert('Time is up!');
+    }
+    $('#counter').text(counter);
+  }, 1000);
+}
+
+$('#startMazeBtn').click(() => {
+  startCounter();
+ // makeMaze();
+});
+
   function displayVictoryMess(moves) {
     document.getElementById("moves").innerHTML = "You Moved " + moves + " Steps.";
     toggleVisablity("Message-Container");  
@@ -488,74 +506,74 @@ function rand(max) {
   var maze, draw, player;
   var cellSize;
   var difficulty;
-  // sprite.src = 'media/sprite.png';
+  sprite.src = 'media/sprite.png';
   
-  // window.onload = function() {
-  //   let viewWidth = $("#view").width();
-  //   let viewHeight = $("#view").height();
-  //   if (viewHeight < viewWidth) {
-  //     ctx.canvas.width = viewHeight - viewHeight / 100;
-  //     ctx.canvas.height = viewHeight - viewHeight / 100;
-  //   } else {
-  //     ctx.canvas.width = viewWidth - viewWidth / 100;
-  //     ctx.canvas.height = viewWidth - viewWidth / 100;
-  //   }
+  window.onload = function() {
+    let viewWidth = $("#view").width();
+    let viewHeight = $("#view").height();
+    if (viewHeight < viewWidth) {
+      ctx.canvas.width = viewHeight - viewHeight / 100;
+      ctx.canvas.height = viewHeight - viewHeight / 100;
+    } else {
+      ctx.canvas.width = viewWidth - viewWidth / 100;
+      ctx.canvas.height = viewWidth - viewWidth / 100;
+    }
   
-  //   //Load and edit sprites
-  //   var completeOne = false;
-  //   var completeTwo = false;
-  //   var isComplete = () => {
-  //     if(completeOne === true && completeTwo === true)
-  //        {
-  //          console.log("Runs");
-  //          setTimeout(function(){
-  //            makeMaze();
-  //          }, 500);         
-  //        }
-  //   };
-  //   sprite = new Image();
-  //   sprite.src =
-  //     "./key.png" +
-  //     "?" +
-  //     new Date().getTime();
-  //   sprite.setAttribute("crossOrigin", " ");
-  //   sprite.onload = function() {
-  //     sprite = changeBrightness(1.2, sprite);
-  //     completeOne = true;
-  //     console.log(completeOne);
-  //     isComplete();
-  //   };
+    //Load and edit sprites
+    var completeOne = false;
+    var completeTwo = false;
+    var isComplete = () => {
+      if(completeOne === true && completeTwo === true)
+         {
+           console.log("Runs");
+           setTimeout(function(){
+             makeMaze();
+           }, 500);         
+         }
+    };
+    sprite = new Image();
+    sprite.src =
+      "./key.png" +
+      "?" +
+      new Date().getTime();
+    sprite.setAttribute("crossOrigin", " ");
+    sprite.onload = function() {
+      sprite = changeBrightness(1.2, sprite);
+      completeOne = true;
+      console.log(completeOne);
+      isComplete();
+    };
   
-  //   finishSprite = new Image();
-  //   finishSprite.src = "./home.png"+
-  //   "?" +
-  //   new Date().getTime();
-  //   finishSprite.setAttribute("crossOrigin", " ");
-  //   finishSprite.onload = function() {
-  //     finishSprite = changeBrightness(1.1, finishSprite);
-  //     completeTwo = true;
-  //     console.log(completeTwo);
-  //     isComplete();
-  //   };
+    finishSprite = new Image();
+    finishSprite.src = "./home.png"+
+    "?" +
+    new Date().getTime();
+    finishSprite.setAttribute("crossOrigin", " ");
+    finishSprite.onload = function() {
+      finishSprite = changeBrightness(1.1, finishSprite);
+      completeTwo = true;
+      console.log(completeTwo);
+      isComplete();
+    };
     
-  // };s
+  };s
   
-  // window.onresize = function() {
-  //   let viewWidth = $("#view").width();
-  //   let viewHeight = $("#view").height();
-  //   if (viewHeight < viewWidth) {
-  //     ctx.canvas.width = viewHeight - viewHeight / 100;
-  //     ctx.canvas.height = viewHeight - viewHeight / 100;
-  //   } else {
-  //     ctx.canvas.width = viewWidth - viewWidth / 100;
-  //     ctx.canvas.height = viewWidth - viewWidth / 100;
-  //   }
-  //   cellSize = mazeCanvas.width / difficulty;
-  //   if (player != null) {
-  //     draw.redrawMaze(cellSize);
-  //     player.redrawPlayer(cellSize);
-  //   }
-  // };
+  window.onresize = function() {
+    let viewWidth = $("#view").width();
+    let viewHeight = $("#view").height();
+    if (viewHeight < viewWidth) {
+      ctx.canvas.width = viewHeight - viewHeight / 100;
+      ctx.canvas.height = viewHeight - viewHeight / 100;
+    } else {
+      ctx.canvas.width = viewWidth - viewWidth / 100;
+      ctx.canvas.height = viewWidth - viewWidth / 100;
+    }
+    cellSize = mazeCanvas.width / difficulty;
+    if (player != null) {
+      draw.redrawMaze(cellSize);
+      player.redrawPlayer(cellSize);
+    }
+  };
   
   function makeMaze() {
     if (player != undefined) {
